693b36c40d477778c9b72030904f761b2d8e29e5
NotExtazy <164105625+NotExtazy@users.noreply.github.com>
2026-01-13 14:48:17 +0800

    Refactor shareholder info layout and add search feature
    Updated button structure and added search bar for shareholder names.

--- shareholderinfo.php
+++ shareholderinfo.php
@@ -24,7 +24,6 @@
             }
         
             form {
-        
                 padding: 20px;
                 background-color: white;
                 border-radius: 10px;
@@ -50,6 +49,7 @@
                 background-color: lightgray;
             }
 
+            /* Names list styles */
             #shNamesList {
                 border: 1px solid #ced4da;
                 border-radius: 0.375rem;
@@ -95,22 +95,15 @@
                     <div>
                         <div class="shadow p-3 rounded-2" style="background-color: white;overflow:auto;">
                             <div class="align-items-center justify-content-between mb-3">
-                                <button id="ConfigurationBtn" class="btn btn-secondary  px-3 py-2 float-end mx-2" type="button"><i class="fa-solid fa-gear"></i> Configuration</button>
+                                <button id="ConfigurationBtn" class="btn btn-secondary  px-3 py-2 float-end mx-2" type="button">
+                                    <i class="fa-solid fa-gear"></i> Configuration
+                                </button>
 
                                 <p class="fw-medium fs-5 mb-4" style="color: #090909;">Shareholder List</p>
                             </div>
                             <hr style="height: 1px">
-                            <div class="row">
-                                <div class="form-group row mb-3">
-                                    <label class="col-sm-auto col-form-label">Name</label>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-sm" id="shNames" placeholder="Search shareholder name..." autocomplete="off">
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="shNamesList" class="list-group mb-3">
-                                <!-- Names will be populated here -->
-                            </div>
+
+                            <!-- TABLE FIRST -->
                             <table id="shareholderTbl" class="table table-bordered text-center" style="width:100%;">
                                 <thead>
                                     <tr>
@@ -124,6 +117,23 @@
                                 <tbody id="shareholderList">
                                 </tbody>
                             </table>
+
+                            <!-- SEARCH BAR + NAMES LIST UNDER THE TABLE -->
+                            <div class="row mt-3">
+                                <div class="form-group row mb-3">
+                                    <label class="col-sm-auto col-form-label">Name</label>
+                                    <div class="col-sm-6">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               id="shNames"
+                                               placeholder="Search shareholder name..."
+                                               autocomplete="off">
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="shNamesList" class="list-group mb-3">
+                                <!-- Names will be populated here by JS -->
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -132,11 +142,17 @@
                     <div class="col-md-12 mb-2">
                         <form class="p-3 needs-validation shadow mb-4" novalidate method="POST" id="shareholderInfo">
                             <div class=" align-items-center justify-content-between mb-3">
-                                <button type="button" name="printCert" id="printCert" class="btn btn-info px-3 py-2 float-end" onclick="PrintReport();" disabled><i class="fa fa-print"></i> Generate Certificate</button>
+                                <button type="button" name="printCert" id="printCert" class="btn btn-info px-3 py-2 float-end" onclick="PrintReport();" disabled>
+                                    <i class="fa fa-print"></i> Generate Certificate
+                                </button>
 
-                                <button id="editButton" class="btn btn-primary  px-3 py-2 float-end mx-2" type="button"  disabled><i class="fa-solid fa-pen-to-square"></i> Edit</button>
+                                <button id="editButton" class="btn btn-primary  px-3 py-2 float-end mx-2" type="button"  disabled>
+                                    <i class="fa-solid fa-pen-to-square"></i> Edit
+                                </button>
 
-                                <button id="addNew" class="btn btn-success  px-3 py-2 float-end mx-2" type="button"  ><i class="fa-solid fa-plus"></i> New</button>
+                                <button id="addNew" class="btn btn-success  px-3 py-2 float-end mx-2" type="button">
+                                    <i class="fa-solid fa-plus"></i> New
+                                </button>
 
                                 <p class="fw-medium fs-5 mb-4" style="color: #090909;">Shareholder's Information</p>
                             </div>
@@ -173,7 +189,14 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <label for="shareholderName" class="form-label mt-2">Shareholder Name</label>
-                                    <input type="text" class="form-control searchName" id="shareholderName" name="shareholderName" placeholder="Shareholder Name" required autocomplete="off" disabled>
+                                    <input type="text"
+                                           class="form-control searchName"
+                                           id="shareholderName"
+                                           name="shareholderName"
+                                           placeholder="Shareholder Name"
+                                           required
+                                           autocomplete="off"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Please provide your shareholder name.
                                     </div>
@@ -181,7 +204,13 @@
 
                                 <div class="col-md-4">
                                     <label for="contact_number" class="form-label mt-2">Contact No.</label>
-                                    <input type="text" class="form-control" id="contact_number" name="contact_number" placeholder="09*********" maxlength="11"  disabled>
+                                    <input type="text"
+                                           class="form-control"
+                                           id="contact_number"
+                                           name="contact_number"
+                                           placeholder="09*********"
+                                           maxlength="11"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Please provide valid contact no.
                                     </div>
@@ -189,7 +218,12 @@
 
                                 <div class="col-md-4">
                                     <label for="email" class="form-label mt-2">Email Address</label>
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="example@gmail.com"  disabled>
+                                    <input type="email"
+                                           class="form-control"
+                                           id="email"
+                                           name="email"
+                                           placeholder="example@gmail.com"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Enter your Email Address.
                                     </div>
@@ -197,7 +231,12 @@
 
                                 <div class="col-md-4">
                                     <label for="facebook_account" class="form-label mt-2">Facebook Link</label>
-                                    <input type="text" class="form-control" id="facebook_account" name="facebook_account" placeholder="https://www.facebook.com/juan"  disabled>
+                                    <input type="text"
+                                           class="form-control"
+                                           id="facebook_account"
+                                           name="facebook_account"
+                                           placeholder="https://www.facebook.com/juan"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Please provide your Facebook Link.
                                     </div>
@@ -207,20 +246,24 @@
                                     <label for="shareholder_type" class="form-label mt-2">Shareholder Type:</label>
                                     <select class="form-select" aria-label="Default select example" id="shareholder_type" name="shareholder_type" disabled>
                                         <option value="" selected>Select</option>
-                                        
                                     </select>
                                 </div>
                                 <div class="col-md-4">
                                     <label for="type" class="form-label mt-2">Type of Shares:</label>
                                     <select class="form-select" aria-label="Default select example" id="type" name="type" disabled>
                                         <option value="" selected>Select</option>
-                                    
                                     </select>
                                 </div>
 
                                 <div class="col-md-4">
                                     <label for="noofshare" class="form-label mt-2">No of Shares:</label>
-                                    <input type="number" class="form-control" id="noofshare" name="noofshare" placeholder="" required oninput="calculateAmount()" disabled>
+                                    <input type="number"
+                                           class="form-control"
+                                           id="noofshare"
+                                           name="noofshare"
+                                           required
+                                           oninput="calculateAmount()"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Please provide your No of Shares.
                                     </div>
@@ -228,7 +271,11 @@
 
                                 <div class="col-md-4">
                                     <label for="amount_share" class="form-label mt-2">Amount of Shares:</label>
-                                    <input type="number" class="form-control" id="amount_share" name="amount_share" placeholder=""  disabled>
+                                    <input type="number"
+                                           class="form-control"
+                                           id="amount_share"
+                                           name="amount_share"
+                                           disabled>
                                     <div class="invalid-feedback">
                                         Please provide your Amount of Shares.
                                     </div>
@@ -246,11 +293,32 @@
                             
                             <div class="row ">
                                 <div class="col-12 text-end mt-3">
-                                    <button id="updateButton" disabled class="btn btn-primary mx-2 float-end" style="display: none;" type="button" form="shareholderInfo"><i class="fa-solid fa-upload"></i>Update</button>
+                                    <button id="updateButton"
+                                            disabled
+                                            class="btn btn-primary mx-2 float-end"
+                                            style="display: none;"
+                                            type="button"
+                                            form="shareholderInfo">
+                                        <i class="fa-solid fa-upload"></i>Update
+                                    </button>
 
-                                    <button id="submitButton" name = "submitButton" disabled class="btn btn-primary mx-2 float-end" type="button" form="shareholderInfo"><i class="fa-solid fa-check-circle"></i> Submit</button>
+                                    <button id="submitButton"
+                                            name="submitButton"
+                                            disabled
+                                            class="btn btn-primary mx-2 float-end"
+                                            type="button"
+                                            form="shareholderInfo">
+                                        <i class="fa-solid fa-check-circle"></i> Submit
+                                    </button>
 
-                                    <button class="btn btn-danger float-end" type="button" id="cancel" hidden disabled onclick="Cancel();"><i class="fa-regular fa-circle-xmark"></i> Cancel</button>
+                                    <button class="btn btn-danger float-end"
+                                            type="button"
+                                            id="cancel"
+                                            hidden
+                                            disabled
+                                            onclick="Cancel();">
+                                        <i class="fa-regular fa-circle-xmark"></i> Cancel
+                                    </button>
                                 </div>
                             </div>
                         </form>
@@ -271,35 +339,62 @@
                                 <div class="form-group row">
                                     <label class="form-label">Signatory 1:</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-sm" name="signatory1Name" id="signatory1Name" placeholder="Enter Name">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatory1Name"
+                                               id="signatory1Name"
+                                               placeholder="Enter Name">
                                     </div>
                                     <div class="col-sm-4">
-                                        <input type="text" class="form-control form-control-sm" name="signatory1Desig" id="signatory1Desig" placeholder="Enter Designation">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatory1Desig"
+                                               id="signatory1Desig"
+                                               placeholder="Enter Designation">
                                     </div>
                                 </div>
                                 <div class="form-group row mt-1">
                                     <label class="form-label">Signatory 2:</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-sm" name="signatory2Name" id="signatory2Name" placeholder="Enter Name">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatory2Name"
+                                               id="signatory2Name"
+                                               placeholder="Enter Name">
                                     </div>
                                     <div class="col-sm-4">
-                                        <input type="text" class="form-control form-control-sm" name="signatory2Desig" id="signatory2Desig" placeholder="Enter Designation">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatory2Desig"
+                                               id="signatory2Desig"
+                                               placeholder="Enter Designation">
                                     </div>
                                 </div>
                                 <div class="form-group row mt-1">
                                     <label class="form-label">Signatory Sub 2:</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-sm" name="signatorySub2Name" id="signatorySub2Name" placeholder="Enter Name">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatorySub2Name"
+                                               id="signatorySub2Name"
+                                               placeholder="Enter Name">
                                     </div>
                                     <div class="col-sm-4">
-                                        <input type="text" class="form-control form-control-sm" name="signatorySub2Desig" id="signatorySub2Desig" placeholder="Enter Designation">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="signatorySub2Desig"
+                                               id="signatorySub2Desig"
+                                               placeholder="Enter Designation">
                                     </div>
                                 </div>
 
                                 <div class="form-group row mt-3">
                                     <label class="form-label">Current Certificate No:</label>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-sm" name="currentCertNo" id="currentCertNo">
+                                        <input type="text"
+                                               class="form-control form-control-sm"
+                                               name="currentCertNo"
+                                               id="currentCertNo">
                                     </div>
                                 </div>
                             </form>
